31bd33023c9e1cbc824ac7302c88bbdb3ff76df6
bacloud14.ubu <bacloud14@gmail.com>
2022-04-26 17:42:40 +0200

    client

--- chat_client/src/Container.jsx
+++ chat_client/src/Container.jsx
@@ -1,3 +1,25 @@
+import { useContext, useEffect, useState } from "react"
+import { io } from "socket.io-client"
+import Context from "./Context"
+
 export default function Container () {
-    return <div></div>
+    const {user, setUser} = useContext(Context)
+
+    const [socket, setSocket] = useState(null)
+
+    useEffect(() => {
+        const newSocket = new io('http://localhost:5000')
+        setSocket(socket)
+        setUser(null)
+        return () => newSocket.close()
+    }, [setSocket])
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-3x1 font-bold text-center bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-b py-3 mb-5">
+                {user ? `Chatting as ${user.name}`: 'Chat as ...'}
+            </h1>
+            {user && socket && socket.connected ? (<div>connected</div>) : null}
+            {!user && socket ? (<div>not connected</div>) : null}
+        </div>
+    )
 }
